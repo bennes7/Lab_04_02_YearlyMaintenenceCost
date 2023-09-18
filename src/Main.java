77371885c9184b523eddca761f1b9f8ac4ac37e3
sarah <bennes7@mail.us.edu>
2023-09-18 15:53:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int fallCost = 301;
+        int winterCost = 100;
+        int springCost = 341;
+        int summerCost = 289;
+        int homeMaintenance = fallCost + winterCost + springCost + summerCost;
+
+        System.out.println("The annual home maintenance cost is " + homeMaintenance + " dollars ");
+    }
+}
